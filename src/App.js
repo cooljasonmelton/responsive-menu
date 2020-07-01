a89361e2c9f93a24792f6c15d3fe146182ce8675
cooljasonmelton <cooljasonmelton>
2020-07-01 17:45:44 -0500

    small navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import SmallNav from './SmallNav';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SmallNav/>
+
 
     </div>
   );
