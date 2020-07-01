def97899c11b8c2f12c4c10fed1a832e022598a1
cooljasonmelton <cooljasonmelton>
2020-07-01 16:48:40 -0500

    react router

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,27 +4,17 @@ import './App.css';
 class Navbar extends React.Component {
     state = { activeItem: "home" }
     handleClick = e => {
-        console.log(e.target.className)
+        console.log(e.target.nav)
+
     }
     render(){
         const { activeItem } = this.state
         return (
             <nav className="navbar">
-                <a 
-                href="#" 
-                className={"nav-item" + activeItem === "home" ? " clicked": ""}
-                onClick={this.handleClick}>
-                    Home
-                </a>
-                <a href="#" className="nav-item stega">
-                    Stegasaurus
-                </a>
-                <a href="#" className="nav-item trex">
-                    T-Rex
-                </a>
-                <a href="#" className="nav-item flyingone?">
-                    Flying One?
-                </a>
+                <button className="nav-item" onClick={this.handleClick} nav="nav">
+                    yo
+                </button>
+
             </nav>
         );
     }
